477bc30fa1e24649da941e3ef9ea00767d743108
Luseniik <lkromah@aggies.ncat.edu>
2015-09-05 12:24:49 -0400

    updates

--- js/food.js
+++ js/food.js
@@ -1,15 +1,9 @@
 var main = function() {
 	"use strict";
 	$("#search").click(function() {
-		
 		var kindOfFood = $('input#query').val();
-		
-		
-		
 		food(kindOfFood);
-
 		$('input#query').val("");
-	
 	});
 }
 
@@ -19,10 +13,8 @@ $(document).ready(main);
 var food = function(kindOfFood){
 		$("#results .list-group").empty();
 var url = "http://api.pearson.com:80/kitchen-manager/v1/recipes?name-contains="+kindOfFood;
-	
 	$.getJSON(url, function(foods) {
 		for (var recipe in foods.results){
-			
 			//console.log(foods.results[recipe]['name']);
 			var $li = $("<li>");
 			$li.addClass("list-group-item");
@@ -34,24 +26,3 @@ var url = "http://api.pearson.com:80/kitchen-manager/v1/recipes?name-contains="+
 		}
 	});
 }
-
-var getSimilarArtist = function(id) {
-	var url = "https://api.spotify.com/v1/artists/" + id + "/related-artists";
-	$.getJSON(url, function(artists) {
-		for (var musician in artists.artists) {
-			console.log(artists.artists[musician]['name']);
-
-			var $li = $("<li>");
-			$li.addClass("list-group-item");
-			$li.text(artists.artists[musician]['name']);
-
-			$li.hide();
-			// 	for(var photo in musician){
-			// 	$img.attr("src",artists[musician][photo]['images'][0]['url']);
-			$("#results .list-group").append($li);
-			$li.slideDown();
-			// }
-		}
-	});
-}
-
